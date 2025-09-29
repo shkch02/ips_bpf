432053b3626365ce53d391dc587c4d5d2980848d
shkch02 <cba7215@gmail.com>
2025-09-29 10:50:59 +0900

    심볼,라이브러리 추출 완료 프린트 정상작동 확인

--- pkg/analyzer/elf_parser.go
+++ pkg/analyzer/elf_parser.go
@@ -11,7 +11,7 @@ type ELFAnalyzer struct {
     elfFile *elf.File
 }
 
-// New는 파일 경로를 받아 새로운 ELFAnalyzer를 생성합니다.
+// New : ELFAnalyzer 구조체 생성
 func New(filePath string) (*ELFAnalyzer, error) {
     elfFile, err := elf.Open(filePath)
     if err != nil {
@@ -20,21 +20,18 @@ func New(filePath string) (*ELFAnalyzer, error) {
     return &ELFAnalyzer{elfFile: elfFile}, nil
 }
 
-// Close는 ELF 파일을 닫습니다. defer와 함께 사용해야 합니다.
+// Close :  ELF 파일을 닫습니다. defer와 함께 사용해야 합니다.
 func (a *ELFAnalyzer) Close() {
     a.elfFile.Close()
 }
 
 
-// ExtractSharedLibs는 지정된 ELF 파일 경로를 받아 해당 파일이 의존하는
-// 공유 라이브러리(.so) 목록을 추출합니다.
+// ExtractSharedLibs :  의존하는 공유 라이브러리 목록을 추출
 func (a *ELFAnalyzer) ExtractSharedLibs() ([]string, error) {
     return a.elfFile.ImportedLibraries()
-    // 에러 처리는 main 쪽에서 더 간단하게 할 수 있습니다.
 }
 
-// ExtractSymbols는 지정된 ELF 파일 경로를 받아 해당 파일에 포함된 심볼 목록을 추출합니다. (스트립 되지 않은 시스템콜을 뽑음)
-// 심볼 :  함수 이름, 변수 이름 등
+// ExtractDynamicSymbols : 동적 심볼 추출
 func (a *ELFAnalyzer) ExtractDynamicSymbols() ([]string, error) {
     dynamicSymbols, err := a.elfFile.DynamicSymbols()
     if err != nil {
@@ -48,7 +45,7 @@ func (a *ELFAnalyzer) ExtractDynamicSymbols() ([]string, error) {
     return symbolNames, nil
 }
 
-//스트립 되지 않은 elf있다면 해당 함수 사용
+// ExtractSymbols : 스트립 되지 않은 elf대상으로 모든 심볼 추출
 func (a *ELFAnalyzer) ExtractSymbols() ([]string, error) {
     Symbols, err := a.elfFile.Symbols()
     if err != nil {
