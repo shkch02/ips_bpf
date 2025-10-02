13d25171437bb5c39f7a4181ff752f71b5170a94
shkch02 <cba7215@gmail.com>
2025-10-02 19:52:09 +0900

    insn.Address,Mnemonic,Opstr만 출력

--- pkg/asmanalysis/syscall_finder.go
+++ pkg/asmanalysis/syscall_finder.go
@@ -25,7 +25,7 @@ func FindSyscalls(instructions []gapstone.Instruction) ([]SyscallInfo, error) {
 		//if insn.X86 == nil {
 		//	fmt.Println("디버깅용 출력:,insn.X86 == nil, continue")
 		//	continue
-		}
+		//}
 		fmt.Println("디버깅용 출력 forloop 내부임 ---------------------") //디버깅용 출력
 		//fmt.Println("디버깅용 출력:insn.Id", insn.Id)
 		fmt.Println("디버깅용 출력:insn.Address", insn.Address)
