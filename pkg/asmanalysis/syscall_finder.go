fd49d9fa8c2ede00e695a42894a9279973f2b07e
shkck02 <cba7215@gmail.com>
2025-10-04 16:48:32 +0900

    result 디버깅 코드 처리

--- pkg/asmanalysis/syscall_finder.go
+++ pkg/asmanalysis/syscall_finder.go
@@ -69,7 +69,7 @@ func FindSyscalls(SyscallAddr uint64, instructions []gapstone.Instruction) ([]Sy
 		// --- 2. syscall 명령어 탐지 ---
 
 		var indirectcall bool = (SyscallAddr == uint64(insn.Address)+uint64(insn.X86.Operands[0].Mem.Disp)+6)
-		var directcall bool = true
+		var directcall bool = false
 		var iscall bool = (insn.Mnemonic == "call")
 
 		//디버그
