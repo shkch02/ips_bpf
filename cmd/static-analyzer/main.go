04c4a64086a332b23dfb25c0fb59a4966d1f49bc
shkck02 <cba7215@gmail.com>
2025-10-04 20:48:43 +0900

    리로케이션 목록 추출까지는 끝남 ㅎㅎ

--- cmd/static-analyzer/main.go
+++ cmd/static-analyzer/main.go
@@ -9,7 +9,7 @@ import (
 	"debug/elf"
 	"fmt"
 	"ips_bpf/static-analyzer/pkg/analyzer"
-	"ips_bpf/static-analyzer/pkg/asmanalysis"
+	//"ips_bpf/static-analyzer/pkg/asmanalysis"
 	"log"
 	"os"
 )
@@ -106,10 +106,10 @@ func main() {
 	syscalls, err := asmanalysis.FindSyscalls(SyscallAddr, insns)
 	if err != nil {
 		log.Printf("systemcall 추출중 오류 발생: %v", err)
-	}*/
+	}
 
 	fmt.Println("발견된 시스템 콜 목록:")
 	for _, sc := range syscalls {
 		fmt.Printf("- 주소: 0x%x, 시스템 콜 번호: %d\n", sc.Address, sc.Number)
-	}
+	}*/
 }
