fb7cbaa291e5df7fd94b54b650c398cc4c342fff
shkch02 <cba7215@gmail.com>
2025-11-18 19:05:34 +0900

    도커파일 주석처리

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -10,10 +10,10 @@ const LibcPath = "./libc.so.6"
 
 // LoadRedisAddr는 환경 변수에서 Redis 주소를 로드합니다.
 func LoadRedisAddr() string {
-	redisAddr := os.Getenv("REDIS_ADDR")
+	redisAddr := os.Getenv("CCSL_REDIS_ADDR")
 	if redisAddr == "" {
 		redisAddr = "localhost:6379" // 환경 변수가 없으면 localhost를 기본값으로 사용
-		log.Printf("[정보] REDIS_ADDR 환경 변수가 설정되지 않았습니다. 기본값(localhost:6379)으로 연결 시도...")
+		log.Printf("[정보] CCSL_REDIS_ADDR 환경 변수가 설정되지 않았습니다. 기본값(localhost:6379)으로 연결 시도...")
 	}
 	return redisAddr
 }
@@ -21,5 +21,5 @@ func LoadRedisAddr() string {
 // [신규] LoadRedisPassword는 환경 변수에서 Redis 비밀번호를 로드합니다.
 func LoadRedisPassword() string {
 	// Job 매니페스트에서 Secret을 통해 주입될 것입니다.
-	return os.Getenv("REDIS_PASSWORD")
+	return os.Getenv("CCSL_REDIS_PASSWORD")
 }
